2d0e5be73f2d5f45c1c65072b11bfbf6c3d954ba
GalacticPasto <adhikarybrizein2003@gmail.com>
2024-10-26 23:10:33 -0400

    lazy config

---
+++ lua/past/lazy/formatter.lua
@@ -0,0 +1,32 @@
+return {
+    "stevearc/conform.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local conform = require("conform")
+
+        conform.setup({
+            formatters_by_ft = {
+                c = { "my_formatter" },
+                cpp = { "my_formatter" },
+            },
+            format_on_save = {
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 1000,
+            },
+        })
+        formatters = {
+            my_formatter = {
+                command = 'clang-format',
+                args = '-style=Microsoft',
+            }, 
+        }
+        vim.keymap.set({ "n", "v" }, "<leader>f", function()
+            conform.format({
+                lsp_fallback = true,
+                async = false,
+                timeout_ms = 1000,
+            })
+        end, { desc = "Format file or range (in visual mode)" })
+    end,
+}
---
+++ lua/past/remap.lua
@@ -0,0 +1,55 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
+-- This is going to get me cancelled
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+--vim.keymap.set("n", "<leader>f", function()
+--    local cursor = vim.fn.getcursorcharpos('.')
+--    vim.cmd("!clang-format -style=Microsoft -i %")
+--    vim.fn.setcursorcharpos(cursor[2],cursor[3])
+--end)
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", function()
+    vim.cmd("!./build.sh")
+end)
+vim.keymap.set("n", "<leader>r", function()
+    vim.cmd("!cd build && ./*")
+end)
+
+vim.keymap.set("n", "<leader>D", function()
+    vim.cmd("!cd build && gf2 ./*")
+end)
+
+
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
+
